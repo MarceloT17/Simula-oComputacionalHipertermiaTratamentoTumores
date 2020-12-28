5e5dc61ac0ffafbbdface79a9d5a1bfbe88e4b01
MarceloT17 <70712869+MarceloT17@users.noreply.github.com>
2020-12-28 15:33:54 -0300

    Create SolTmms13_1FH.py

---
+++ CodeST/CodeSTFibrasHorizontal/SolTmms13_1FH.py
@@ -0,0 +1,34 @@
+#Isotropico com k = 0.485, e sem tumor
+# 1 significa sem tumor
+#O valor da condutividade termica nao afeta, sendo essa copia do SolTmms_1_k1, isotropico
+
+import numpy
+import math
+import matplotlib.pyplot as plt
+import seaborn
+
+Z = 0.1
+R = 0.03
+ra = 0.01
+
+nr = 80
+nz = 100
+
+T = numpy.zeros([nr, nz], dtype=float)
+
+for i in range(0, nr, 1):
+    for j in range(0, nz, 1):
+        T[i, j] = (math.sin((((i + (1.0 / 2.0)) * ((R - ra) / nr)) + ra) * (math.pi) / (R - ra))) * (math.sin(((j + (1.0 / 2.0)) * (Z / nz)) * (math.pi) / Z))
+
+print(T)
+
+
+A_sol13 = T.copy()
+'''
+ncolunas1 = (nr * nz)
+eixoD1 = numpy.linspace(ra, R, ncolunas1)
+A_1 = A_sol12.copy()
+A_1_1 = numpy.reshape(A_1, ncolunas1)
+plt.plot(eixoD1, A_1_1)
+plt.show()
+'''
